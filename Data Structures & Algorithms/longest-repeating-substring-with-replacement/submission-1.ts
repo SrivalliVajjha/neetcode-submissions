32580c55e42a80b17109700efbfa753ce84ee785
SrivalliVajjha <valli.sri1996@gmail.com>
2026-07-26 19:47:43 -0400

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        let l=0;
+        let freq = new Map<string, number>();
+        let maxCount = 0;
+        let maxFreq=0;
+
+        for(let r=0; r< s.length; r++){
+            freq.set(s[r],(freq.get(s[r]) ||0) +1);
+            maxFreq = Math.max(freq.get(s[r])!,maxFreq);
+
+            while( (r-l+1) - maxFreq > k) { // failing
+                freq.set(s[l], freq.get(s[l])! - 1);
+                l++;
+            }
+            
+            maxCount = Math.max(maxCount,r-l+1);
+        }
+        return maxCount;
+    }
+}
